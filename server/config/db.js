ee9a9ad6b0d7d54b3703c32a1f3ff56ee7b64254
Harshitha019csd <harshitha.ls.2024.csd@rajalakshmi.edu.in>
2026-03-17 08:15:18 +0530

    attack_info

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/securityDB");
+        const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/securityDB";
+        await mongoose.connect(uri);
         console.log("MongoDB connected");
     } catch (error) {
         console.log("Database connection error:", error);
